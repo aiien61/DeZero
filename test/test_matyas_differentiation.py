44f0145dd68d9ac3e768165c8b534cf94ce9b6af
aiien61 <aiien61will@gmail.com>
2021-11-29 23:44:20 +0800

    update

---
+++ test/test_matyas_differentiation.py
@@ -0,0 +1,19 @@
+if '__file__' in globals():
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+
+
+def matyas(x, y):
+    z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
+    return z
+
+
+x = Variable(np.array(1.0))
+y = Variable(np.array(1.0))
+z = matyas(x, y)
+z.backward()
+print(x.grad, y.grad)
