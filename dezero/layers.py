aed8730ee08e51000580cf1d1b74aa4286f8a2ff
aiien61 <aiien61will@gmail.com>
2021-12-20 00:59:18 +0800

    update

--- dezero/layers.py
+++ dezero/layers.py
@@ -36,16 +36,27 @@ class Layer:
 
 
 class Linear(Layer):
-    def __init__(self, in_size, out_size, nobias=False, dtype=np.float32):
+    def __init__(self, out_size, nobias=False, dtype=np.float32, in_size=None):
         super().__init__()
+        self.in_size = in_size
+        self.out_size = out_size
+        self.dtype = dtype
+        self.W = Parameter(None, name="W")
+        if self.in_size is not None:
+            self._init_W()
 
-        I, O = in_size, out_size
-        W_data = np.random.randn(I, O).astype(dtype) * np.sqrt(1/I)
-        self.W = Parameter(W_data, name='W')
         if nobias:
             self.b = None
         else:
             self.b = Parameter(np.zeros(0, dtype=dtype), name='b')
+
+    def _init_W(self):
+        I, O = self.in_size, self.out_size
+        W_data = np.random.randn(I, O).astype(self.dtype) * np.sqrt(1/I)
+        self.W.data = W_data
     
     def forward(self, x):
+        if self.W.data is None:
+            self.in_size = x.shape[1]
+            self._init_W()
         return F.linear(x, self.W, self.b)
--- test/test_layer.py
+++ test/test_layer.py
@@ -5,6 +5,8 @@ if '__file__' in globals():
 
 import numpy as np
 from dezero import Variable, Parameter, Layer
+import dezero.functions as F
+import dezero.layers as L
 
 x = Variable(np.array(1.0))
 p = Parameter(np.array(2.0))
@@ -27,3 +29,34 @@ print('-------------')
 
 for name in layer._params:
     print(name, layer.__dict__[name])
+
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
+
+l1 = L.Linear(10)
+l2 = L.Linear(1)
+
+def predict(x):
+    y = l1(x)
+    y = F.sigmoid(y)
+    y = l2(y)
+    return y
+
+lr = 0.2
+iters = 10000
+
+for i in range(iters):
+    y_pred = predict(x)
+    loss = F.mean_squared_error(y, y_pred)
+
+    l1.cleargrads()
+    l2.cleargrads()
+    loss.backward()
+
+    for l in [l1, l2]:
+        for p in l.params():
+            p.data -= lr * p.grad.data
+
+    if i % 1000 == 0:
+        print(loss)
