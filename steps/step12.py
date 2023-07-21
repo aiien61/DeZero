74c48ed06c0af73a4130dd1b5bbed2f2d357740f
aiien61 <aiien61will@gmail.com>
2023-07-21 21:55:04 +0800

    update

--- steps/step12.py
+++ steps/step12.py
@@ -1,11 +1,13 @@
 import numpy as np
-from step09 import Variable, as_array
+from step09 import as_array, Variable
 
 
 class Function:
     def __call__(self, *inputs):
         xs = [x.data for x in inputs]
-        ys = self.forward(xs)
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
 
         for output in outputs:
@@ -14,24 +16,24 @@ class Function:
         self.inputs = inputs
         self.outputs = outputs
         return outputs if len(outputs) > 1 else outputs[0]
-    
+
     def forward(self, xs):
         raise NotImplementedError()
-    
+
     def backward(self, gys):
         raise NotImplementedError()
 
 
 class Add(Function):
-    def forward(self, xs):
-        x0, x1 = xs
-        y = x0 + x1
-        return (y,)
+    def forward(self, x0, x1):
+        return x0 + x1
 
-    
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
 if __name__ == "__main__":
-    xs = [Variable(as_array(2)), Variable(as_array(3))]
-    f = Add()
-    ys = f(xs)
-    y = ys[0]
+    x0 = Variable(np.array(2))
+    x1 = Variable(np.array(3))
+    y = add(x0, x1)
     print(y.data)
