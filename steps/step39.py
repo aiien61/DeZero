dfb56de37a2f163a74162f5d8dde88d0d329120d
aiien61 <aiien61will@gmail.com>
2023-10-11 23:48:02 +0800

    scaffolding in testcases

--- steps/step39.py
+++ steps/step39.py
@@ -13,10 +13,13 @@ class Test(unittest.TestCase):
     testcases: dict = {
         'x_1d': Variable(np.array([1, 2, 3, 4, 5, 6])),
         'x_2d': Variable(np.array([[1, 2, 3], [4, 5, 6]])),
+
         'y_sum_axis_None': Variable(np.array(21)),
         'y_sum_axis_0': Variable(np.array([5, 7, 9])),
         'y_sum_axis_1': Variable(np.array([6, 15])),
+
         'y_sum_keepdims': Variable(np.array([[21]])),
+
         'x_grad_1d': Variable(np.array([1, 1, 1, 1, 1, 1])),
         'x_grad_2d': Variable(np.array([[1, 1, 1], [1, 1, 1]]))
     }
@@ -24,104 +27,138 @@ class Test(unittest.TestCase):
     def test_1D_sum(self):
         print('test_1D_sum')
         x = self.testcases['x_1d']
-        y = F.sum(x)
-        y.backward()
+        print('x:', x)
         
-        print(y)
+        y = F.sum(x)
+        print('y:', y)
+
+        # ------------------------------------------------------------
         expected = self.testcases['y_sum_axis_None'].data
         actual = y.data
+        print('expected y.data:', expected)
+        print('actual y.data:', actual)
         self.assertTrue(actual == expected)
 
-        print(x.grad)
+        # ------------------------------------------------------------
+        y.backward()
+
+        # ------------------------------------------------------------
         expected = self.testcases['x_grad_1d'].data
         actual = x.grad.data
+
+        print('expected x.grad:', expected)
+        print('actual x.grad:', actual)
         self.assertTrue((actual == expected).all())
 
-        print(x.grad.shape)
+        # ------------------------------------------------------------
         expected = self.testcases['x_grad_1d'].shape
-        actual = x.shape
+        actual = x.grad.shape
+        print('expected x.grad.shape:', expected)
+        print('actual x.grad.shape:', actual)
         self.assertTrue(actual == expected)
 
 
     def test_2D_sum_with_axis(self):
         print('\ntest_2D_sum_with_axis')
         x = self.testcases['x_2d']
+        print('x:', x)
 
         # ------------------------------------------------------------
         y = F.sum(x, axis=0)
+        print('column-wise sum y:', y)
+
         y.backward()
-        
-        print(y)
+
         expected = self.testcases['y_sum_axis_0'].data
         actual = y.data
+        print('expected y.data:', expected)
+        print('actual y.data', actual)
         self.assertTrue(all(actual == expected))
 
-        print(x.grad)
         expected = self.testcases['x_grad_2d'].data
         actual = x.grad.data
+        print('expected x.grad:', expected)
+        print('actual x.grad:', actual)
         self.assertTrue((actual == expected).all())
 
-        print(x.grad.shape)
         expected = self.testcases['x_grad_2d'].shape
         actual = x.shape
+        print('expected x.grad.shape:', expected)
+        print('actual x.grad.shape:', actual)
         self.assertTrue(actual == expected)
 
         # ------------------------------------------------------------
         x.cleargrad()
         y = F.sum(x, axis=1)
+        print('x:', x)
+        print('row-wise sum y:', y)
+
         y.backward()
 
-        print(y)
         expected = self.testcases['y_sum_axis_1'].data
         actual = y.data
+        print('expected y.data:', expected)
+        print('actual y.data:', actual)
         self.assertTrue((actual == expected).all())
 
-        print(x.grad)
         expected = self.testcases['x_grad_2d'].data
         actual = x.grad.data
+        print('expected x.grad:', expected)
+        print('actual x.grad:', actual)
         self.assertTrue((actual == expected).all())
 
-        print(x.grad.shape)
         expected = self.testcases['x_grad_2d'].shape
         actual = x.shape
+        print('expected x.grad.shape:', expected)
+        print('actual x.grad.shape:', actual)
         self.assertTrue(actual == expected)
     
     
     def test_2D_sum_with_keepdims(self):
         print('\ntest_2D_sum_with_keepdims')
         x = self.testcases['x_2d']
+        print('x:', x)
         x.cleargrad()
         y = x.sum(keepdims=True)
+        print('y with dims kept:', y)
+
         y.backward()
         
-        print(y)
         expected = self.testcases['y_sum_keepdims'].data
         actual = y.data
+        print('expected y.data:', expected)
+        print('actual y.data:', actual)
         self.assertTrue((actual == expected).all())
         
-        print(y.shape)
         expected = self.testcases['y_sum_keepdims'].shape
         actual = y.shape
+        print('expected y.shape:', expected)
+        print('actual y.shape:', actual)
         self.assertTrue(actual == expected)
         
-        print(x.grad)
         expected = self.testcases['x_grad_2d'].data
         actual = x.grad.data
+        print('expected x.grad:', expected)
+        print('actual x.grad:', actual)
         self.assertTrue((actual == expected).all())
 
-        print(x.grad.shape)
         expected = self.testcases['x_grad_2d'].shape
         actual = x.shape
+        print('expected x.grad.shape:', expected)
+        print('actual x.grad.shape:', actual)
         self.assertTrue(actual == expected)
 
 
     def test_multi_dims_sum(self):
         x = Variable(np.random.randn(2, 3, 4, 5))
         y = x.sum(keepdims=True)
+        print('x:', x)
+        print('y:', y)
         
-        print(y.shape)
         expected = Variable(np.random.randn(1, 1, 1, 1)).shape
         actual = y.shape
+        print('expected y.shape:', expected)
+        print('actual y.shape:', actual)
         self.assertEqual(expected, actual)
 
 
