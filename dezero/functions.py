016091d4d6b64a811438ac57eeced3ec4c7fa590
aiien61 <aiien61will@gmail.com>
2023-10-14 21:01:06 +0800

    fixed the bug in MatMul class and created classes for loss function, linear transformation function, and activation function

--- dezero/functions.py
+++ dezero/functions.py
@@ -7,7 +7,9 @@ import numpy as np
 from dezero import utils
 from dezero.core import Function, as_variable
 
-
+# ---------------------------------------------------------
+# basic functions: sin / cos / tanh / exp
+# ---------------------------------------------------------
 class Sin(Function):
     def forward(self, x):
         return  np.sin(x)
@@ -41,7 +43,7 @@ class Tanh(Function):
         return np.tanh(x)
     
     def backward(self, gy):
-        y = self.outputs[0]()
+        y = self.outputs[0]()  # weakref
         gx = gy * (1 - y * y)
         return gx
 
@@ -50,6 +52,24 @@ def tanh(x):
     return Tanh()(x)
 
 
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    
+
+    def backward(self, gy):
+        y = self.outputs[0]()  # weakref
+        gx = y * gy
+        return gx
+
+
+def exp(x):
+    return Exp()(x)
+
+
+# ---------------------------------------------------------
+# tensor operations: reshape / transpose
+# ---------------------------------------------------------
 class Reshape(Function):
     def __init__(self, shape):
         self.shape = shape
@@ -82,6 +102,9 @@ def transpose(x):
     return Transpose()(x)
 
 
+# ---------------------------------------------------------
+# aggregation functions: sum / broadcast_to / sum_to / tile / matmul
+# ---------------------------------------------------------
 class Sum(Function):
     def __init__(self, axis, keepdims):
         self.axis = axis
@@ -168,9 +191,75 @@ class MatMul(Function):
     def backward(self, gy):
         x, W = self.inputs
         gx = matmul(gy, W.T)
-        gw = matmul(x.T, gy)
-        return gx, gw
+        gW= matmul(x.T, gy)
+        return gx, gW
 
 
 def matmul(x, W):
     return MatMul()(x, W)
+
+
+# ---------------------------------------------------------
+# loss functions: mse
+# ---------------------------------------------------------
+class MeanSquaredError(Function):
+    def forward(self, x0, x1):
+        diff = x0 - x1
+        return (diff ** 2).sum() / len(diff)
+    
+    def backward(self, gy):
+        x0, x1 = self.inputs
+        diff = x0 - x1
+        gx0 = gy * diff * (2. / len(diff))
+        gx1 = -gx0
+        return gx0, gx1
+
+
+def mean_squared_error(x0, x1):
+    return MeanSquaredError()(x0, x1)
+
+
+def mean_squared_error_simple(x0, x1):
+    diff = x0 - x1
+    return sum(diff ** 2) / len(diff)
+
+
+# ---------------------------------------------------------
+# transformation functions: linear
+# ---------------------------------------------------------
+class Linear(Function):
+    def forward(self, x, W, b):
+        y = matmul(x, W)
+        if b is not None:
+            y += b
+        return y
+    
+    def backward(self, gy):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW= matmul(x.T, gy)
+        return gx, gW, gb
+
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+
+# Manually release t.data for memory efficiency
+def linear_simple(x, W, b=None):
+    t = matmul(x, W)
+    if b is None:
+        return t
+    
+    y = t + b
+    t.data = None
+    return y
+
+# ---------------------------------------------------------
+# activation functions: sigmoid
+# ---------------------------------------------------------
+def sigmoid_simple(x):
+    x = as_variable(x)
+    y = 1 / (1 + exp(-x))
+    return y
